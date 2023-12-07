2b511cc34d49300a4b529bca8f46a5d4f9ac2bfa
Chris Pennington <cpenn368@gmail.com>
2023-12-06 23:37:15 -0700

    day-2 part-2 done

--- day-2/index.ts
+++ day-2/index.ts
@@ -49,5 +49,51 @@ function runPartOne() {
     }, 0);
 }
 
+function runPartTwo() {
+  return input
+    .trim()
+    .split("\n")
+    .reduce((acc, x) => {
+      // get game index and data into array
+      const splitIndexFromData = x.split(":");
+
+      // get game index
+      const gameIndex = parseInt(splitIndexFromData[0].match(/\d+/g)[0]);
+
+      // get flatted array of all sets
+      const sets = splitIndexFromData[1].split(";");
+      const flattenedSubSets = sets.flatMap((set) => set.split(","));
+
+      // create new map
+      const maxColorsPerCurrentGame = new Map() as Map<string, number>;
+
+      // loop through each game and get max number for each color
+      flattenedSubSets.map((s) => {
+        const key = s.match(/[a-zA-Z]+/)[0];
+        if (maxColorsPerCurrentGame.has(key)) {
+          const value = maxColorsPerCurrentGame.get(key) as number;
+          maxColorsPerCurrentGame.set(
+            key,
+            Math.max(value, parseInt(s.match(/\d+/g)[0]))
+          );
+        } else {
+          maxColorsPerCurrentGame.set(key, parseInt(s.match(/\d+/g)[0]));
+        }
+      });
+
+      // check if any color is greater than max
+      const gamePower = Array.from(maxColorsPerCurrentGame.entries()).reduce(
+        (acc, [_, value]) => {
+          return acc * value;
+        },
+        1
+      );
+
+      return acc + gamePower;
+    }, 0);
+}
+
 const partOneResult = runPartOne();
 console.log("🚀 ~ partOneResult:", partOneResult);
+const partTwoResult = runPartTwo();
+console.log("🚀 ~ partTwoResult:", partTwoResult);
